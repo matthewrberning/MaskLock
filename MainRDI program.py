5549b30347ae4f8d5ed0425fbab8e1b9c5e5fed9
Luis Carpi <luiscarpi1989@gmail.com>
2020-11-29 20:09:38 +0000

    Updated RDI Coding

--- MainRDI program.py
+++ MainRDI program.py
@@ -6,18 +6,29 @@ import socket
 def setup(): ### Set GPIO settings as well as designed voltage/Needs from GPI
     GPIO.setmode(GPIO.BOARD)       # use PHYSICAL GPIO Numbering
     GPIO.setup(ledPin, GPIO.OUT)   # set the ledPin to OUTPUT mode
-    GPIO.output(ledPin, GPIO.low)  # make ledPin output LOW level
+    GPIO.output(ledPin, GPIO.LOW)  # make ledPin output LOW level
     print ('using pin%d'%ledPin)
+def destroy():
+    GPIO.cleanup()                      # Release all GPIO
 
-def hardwarefunction(data):
-    while True:
+def hardwarefunction(code,waittime):
+    if code == '0':
+        print("Null Code - No Mask Detected Seng Signal on this code to Send message to user")
+        
+    
+    elif code == '1':
+        print("CODE 1")
         GPIO.output(ledPin, GPIO.HIGH)  # make ledPin output HIGH level to turn on led
         print ('led turned on >>>')     # print information on terminal
-        time.sleep(3)                   # Wait for 1 second
+        time.sleep(int(waittime))                   # Wait for 1 second
         GPIO.output(ledPin, GPIO.LOW)   # make ledPin output LOW level to turn off led
-        #print ('led turned off <<<')
+        print ('led turned off <<<')
         #time.sleep(1)                   # Wait for 1 second
 
+    elif code == '2':
+       print("CODE 2")
+    else:
+        print("Code not set up for execution on GPIO")
 def get_ip_address():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.connect(("8.8.8.8", 80))
@@ -39,7 +50,7 @@ def setupconnection():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((IPAddr, PORT))
 
-        print("connection lost... reconnecting")
+        print("Starting Connection")
         connected = False
 
         s.listen()
@@ -47,29 +58,35 @@ def setupconnection():
         with conn:
             print('Connected by', addr)
             while True:
-                data = conn.recv(1024)
-                data.decode()
-                print("RECEIVED DATA:" + " TYPE" + str(type(data)) + " Value:" + str(data)
-                hardwarefunction(data))
+
                 try:
-                loop()
+                   data = conn.recv(1024)
+                   data = data.decode()
+                   code = data[0]
+                   waittime = data[-1]
+                   print("RECEIVED DATA:" + " TYPE" + str(type(data)) + " Value:" + str(code) + " wait time:" + waittime)
+                   hardwarefunction(code,waittime)
+                   conn.sendall(bytes([1]))
+                   print(conn.fileno())
+                except ConnectionResetError:
+                    print("Connection Lost Listening Again")
+                    
+                    s.listen()
+                    conn, addr = s.accept()
 
-                except KeyboardInterrupt:   # Press ctrl-c to end the program.
-                    destroy()
-
-                conn.sendall(b"+")
-                print(conn.fileno())
-            else
-                print("User Has exited Connection")
 
 
-def destroy():
-    GPIO.cleanup()                      # Release all GPIO
+            
+            
 
 if __name__ == '__main__':    # Program entrance
     print ('Program is starting ... \n')
     ledPin = 11  # define ledPin
     setup()
-    setupconnection()
+    try:
+        setupconnection()
+    except KeyboardInterrupt:   # Press ctrl-c to end the program.
+        print("Shutting Down Connection")
+        destroy()
 
-
+           
